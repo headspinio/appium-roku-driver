2098aaf22e77e8a0c804698b37fb61329a903107
Christopher Hiller <boneskull@boneskull.com>
2022-10-18 13:39:16 -0700

    chore(): ungulpify
    - adds types, prettier, lint-staged, CI, usual suspects
    - changes `xmldom` dep to `@xmldom/xmldom`

--- test/unit/driver.spec.js
+++ test/unit/driver.spec.js
@@ -1,7 +1,4 @@
 import RokuDriver from '../../lib/driver';
-import chai from 'chai';
-
-const should = chai.should();
 
 describe('driver tests', function () {
   it('should load the driver', function () {
