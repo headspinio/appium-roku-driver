dade09533cf1ce4e00d3d21edbd2a11edc65be23
Christopher Hiller <boneskull@boneskull.com>
2022-10-19 15:34:30 -0700

    chore(): implement typechecks

--- test/functional/caps.js
+++ test/functional/caps.js
@@ -1,6 +1,6 @@
 const rokuHost = process.env.RK_HOST || 'localhost';
 const rokuEcpPort = (process.env.RK_PORT && parseInt(process.env.RK_PORT, 10)) || 8060;
-const rokuWebPort = parseInt(process.env.RK_WEB_PORT || 80, 10);
+const rokuWebPort = parseInt(process.env.RK_WEB_PORT || '80', 10);
 const rokuUser = process.env.RK_USER || 'rokudev';
 const rokuPass = process.env.RK_PASS || '';
 const rokuHeaderHost = process.env.RK_HEADER_HOST || rokuHost;
@@ -17,9 +17,8 @@ const caps = {
   platformName: 'Roku'
 };
 
-const prefixedCaps = Object.keys(caps).reduce((newCaps, key) => {
+export default Object.keys(caps).reduce((newCaps, key) => {
   newCaps[`appium:${key}`] = caps[key];
   return newCaps;
 }, {});
 
-module.exports = prefixedCaps;
