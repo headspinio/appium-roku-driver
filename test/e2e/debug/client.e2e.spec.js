e2734cde04bc9e46febee33bbdd28a500622d80e
Christopher Hiller <boneskull@boneskull.com>
2022-11-03 17:19:35 -0700

    chore(debug-client): update client e2e tests

--- test/e2e/debug/client.e2e.spec.js
+++ test/e2e/debug/client.e2e.spec.js
@@ -1,18 +1,28 @@
+import {pipeline} from 'node:stream/promises';
 import B from 'bluebird';
-import {fs} from 'appium/support';
 import {promisify} from 'node:util';
 import {RokuDebugClient} from '../../../lib/debug/client';
 import {createServer} from 'telnetlib';
 import getPort from 'get-port';
 import unexpectedSinon from 'unexpected-sinon';
 import unexpected from 'unexpected';
+import stoppable from 'stoppable';
 
 const expect = unexpected.clone().use(unexpectedSinon);
 
 const TEST_HOST = '127.0.0.1';
 
-describe('debug log behavior', function () {
-  /** @type {import('net').Server} */
+/**
+ * This is for debugging purposes only, allowing the debugger to pause on a breakpoint
+ * without the connection timing out.
+ * @type {import('telnet-client').ConnectOptions}
+ */
+const BASE_TELNET_OPTS = {
+  timeout: 2147483647
+};
+
+describe('debug log client behavior', function () {
+  /** @type {import('stoppable').StoppableServer} */
   let server;
 
   /** @type {number} */
@@ -25,61 +35,58 @@ describe('debug log behavior', function () {
     if (rdl) {
       await rdl.disconnect();
     }
-    if (server) {
-      await promisify(server.close.bind(server))();
+    if (server?.listening) {
+      await promisify(server.stop.bind(server))();
     }
   });
 
   describe('basic operation', function () {
     beforeEach(async function () {
-      server = createServer({}, (socket) => {
-        socket.on('negotiated', () => {
-          socket.write('HELLO\n');
-        });
-      });
+      server = stoppable(
+        // @ts-expect-error -- stoppable only supports http(s) but not really
+        createServer({}, (socket) => {
+          socket.on('negotiated', () => {
+            socket.write('HELLO\n');
+          });
+        })
+      );
       port = await getPort();
       await promisify(server.listen.bind(server))(port, TEST_HOST);
     });
 
     it('should connect to the device', async function () {
-      rdl = new RokuDebugClient(TEST_HOST, {port});
-      await rdl.connect();
-
-      await expect(rdl.nextData(), 'to be fulfilled with', 'HELLO\n');
+      rdl = new RokuDebugClient(TEST_HOST, {port, telnet: BASE_TELNET_OPTS});
+      await expect(rdl.connect(), 'to be fulfilled');
     });
 
-    it('should write to the log', async function () {
-      rdl = new RokuDebugClient(TEST_HOST, {port});
+    it('should disconnect from the device', async function () {
+      rdl = new RokuDebugClient(TEST_HOST, {port, telnet: BASE_TELNET_OPTS});
       await rdl.connect();
-      await rdl.nextData();
-      await fs.readFile(rdl.logPath, 'utf8').should.eventually.equal('HELLO\n');
-    });
-
-    it('should read some data', async function () {
-      rdl = new RokuDebugClient(TEST_HOST, {port});
-      await rdl.connect();
-      await expect(rdl.nextData(), 'to be fulfilled with', 'HELLO\n');
+      await expect(rdl.disconnect(), 'to be fulfilled');
     });
   });
 
-  describe('with multiple lines', function () {
+  describe('reading from device', function () {
     beforeEach(async function () {
-      server = createServer({}, (socket) => {
-        socket.on('negotiated', async () => {
-          for await (const line of ['HELLO', 'WORLD', '!']) {
-            socket.write(line + '\n');
-            await B.delay(250); // note: this is probably flaky
-          }
-          await promisify(socket.end.bind(socket))();
-        });
-      });
+      server = stoppable(
+        // @ts-expect-error
+        createServer({}, (socket) => {
+          socket.on('negotiated', async () => {
+            for await (const line of ['HELLO', 'WORLD', '!']) {
+              socket.write(line + '\n');
+              await B.delay(250); // note: this is probably flaky
+            }
+            await promisify(socket.end.bind(socket))();
+          });
+        })
+      );
       port = await getPort();
       await promisify(server.listen.bind(server))(port, TEST_HOST);
     });
 
     describe('when disconnected', function () {
-      it('should implement the async iterator protocol', async function () {
-        rdl = new RokuDebugClient(TEST_HOST, {port});
+      it('should implement the async iterable protocol', async function () {
+        rdl = new RokuDebugClient(TEST_HOST, {port, telnet: BASE_TELNET_OPTS});
         const lines = [];
         for await (const line of rdl) {
           lines.push(line);
@@ -90,7 +97,7 @@ describe('debug log behavior', function () {
 
     describe('after disconnection', function () {
       it('should not be connected, obviously', async function () {
-        rdl = new RokuDebugClient(TEST_HOST, {port});
+        rdl = new RokuDebugClient(TEST_HOST, {port, telnet: BASE_TELNET_OPTS});
         await rdl.connect();
         await rdl.disconnect();
         expect(rdl.isConnected, 'to be false');
@@ -98,8 +105,8 @@ describe('debug log behavior', function () {
     });
 
     describe('when connected', function () {
-      it('should implement the async iterator protocol', async function () {
-        rdl = new RokuDebugClient(TEST_HOST, {port});
+      it('should implement the async iteratable protocol', async function () {
+        rdl = new RokuDebugClient(TEST_HOST, {port, telnet: BASE_TELNET_OPTS});
         await rdl.connect();
         const lines = [];
         for await (const line of rdl) {
@@ -108,15 +115,44 @@ describe('debug log behavior', function () {
         expect(lines, 'to equal', ['HELLO', 'WORLD', '!']);
       });
 
-      it('should read the entire logfile', async function () {
-        rdl = new RokuDebugClient(TEST_HOST, {port});
+      it('should be usable in a pipeline', async function () {
+        rdl = new RokuDebugClient(TEST_HOST, {port, telnet: BASE_TELNET_OPTS});
         await rdl.connect();
-        await new B((resolve) => {
-          // we only do this because otherwise we could run into a race condition
-          // waiting for the socket writes
-          rdl.on('finish', resolve);
+        let lines = await pipeline(rdl, async (source) => {
+          const lines = [];
+          for await (const chunk of source) {
+            lines.push(chunk.trimEnd());
+          }
+          return lines;
         });
-        await expect(rdl.getLog(), 'to be fulfilled with', 'HELLO\nWORLD\n!\n');
+        expect(lines, 'to equal', ['HELLO', 'WORLD', '!']);
+      });
+
+      describe('when unexpectedly disconnected', function () {
+        it('should gracefully close', async function () {
+          await promisify(server.stop.bind(server))();
+          server = stoppable(
+            // @ts-expect-error
+            createServer({}, (socket) => {
+              socket.on('negotiated', async () => {
+                await B.delay(250);
+                socket.end();
+              });
+            }),
+            0
+          );
+          port = await getPort();
+          await promisify(server.listen.bind(server))(port, TEST_HOST);
+
+          rdl = new RokuDebugClient(TEST_HOST, {port, telnet: BASE_TELNET_OPTS});
+          await rdl.connect();
+          await expect(
+            new B((resolve) => {
+              rdl.on('close', resolve);
+            }),
+            'to be fulfilled'
+          );
+        });
       });
     });
   });
